61b750027f018dd1003aacb259b21a5ba9920cb2
NuxPower <108980680+NuxPower@users.noreply.github.com>
2024-02-12 14:01:24 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        boolean loop = true;
+        while (loop) {
+            BasicArithmetic.main(args);
+        }
+        
+    }
+}
